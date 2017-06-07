95635321c96b6f5b0b05f649874a3da8ec487209
DESKTOP-OEBT2NS\Victor <victorcarrilho94@hotmail.it>
2017-06-07 15:49:48 +0200

    1 commit , si inizia

---
+++ SiwGalleryProject/src/main/java/it/uniroma3/SiwGalleryProject/SiwGalleryProjectApplication.java
@@ -0,0 +1,12 @@
+package it.uniroma3.SiwGalleryProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwGalleryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwGalleryProjectApplication.class, args);
+	}
+}
